6b2649107756e6caf029b3c9bf439586ca952f24
Wallace Grant <trinia@Wallaces-iMac.local>
2018-10-11 19:07:21 -0400

    watch button text

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Travel for the Black and Queer</h1>
+    <p>There's nothing better than knowing where you can rest in the world</p>
+    <Link to="/page-2/">Watch the Video</Link>
+  </Layout>
+)
+
+export default IndexPage
+     
